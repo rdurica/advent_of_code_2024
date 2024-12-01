dd27409776f88e21657e831a495edd674d9e27c4
Robert Durica <r.durica@gmail.com>
2024-12-01 14:25:33 +0100

    day 01

---
+++ day01/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	firstPart()
+	secondPart()
+}
+
+// firstPart reads pairs of integers from a file, sorts them, and computes the
+// sum of the absolute differences between corresponding elements from two lists.
+func firstPart() {
+	inputFile, err := os.Open("input_01")
+	if err != nil {
+		fmt.Printf("Unable to read puzzle file: %s\n", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(inputFile)
+	scanner.Split(bufio.ScanLines)
+
+	first := []int{}
+	second := []int{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		nums := strings.Split(line, "   ")
+
+		// skip errors, inputs must be correct
+		x, _ := strconv.Atoi(nums[0])
+		y, _ := strconv.Atoi(nums[1])
+
+		first = append(first, x)
+		second = append(second, y)
+	}
+	inputFile.Close()
+
+	slices.Sort(first)
+	slices.Sort(second)
+
+	var sum int = 0
+	for i := 0; i < len(first); i++ {
+		distance := first[i] - second[i]
+		if distance < 0 {
+			sum += distance * -1
+		} else {
+			sum += distance
+		}
+	}
+
+	fmt.Printf("Result of fist part: %d\n", sum)
+}
+
+// secondPart reads pairs of integers from a file, and computes the sum of the
+// occurances of each number in the first column multiplied by the number itself.
+func secondPart() {
+	inputFile, err := os.Open("input_01")
+	if err != nil {
+		fmt.Printf("Unable to read puzzle file: %s", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(inputFile)
+	scanner.Split(bufio.ScanLines)
+
+	first := []int{}
+	occurances := map[int]int{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		nums := strings.Split(line, "   ")
+
+		// skip errors, inputs must be correct
+		x, _ := strconv.Atoi(nums[0])
+		y, _ := strconv.Atoi(nums[1])
+
+		first = append(first, x)
+		occurances[y] = occurances[y] + 1
+	}
+	inputFile.Close()
+
+	var sum int = 0
+	for i := 0; i < len(first); i++ {
+		x := first[i]
+		count := occurances[x]
+
+		sum += x * count
+	}
+
+	fmt.Printf("Result of second part: %d\n", sum)
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Advent of code 2024
+[![Go](https://img.shields.io/badge/Go-%2300ADD8.svg?&logo=go&logoColor=white)](#)
+
+
+Welcome to my Advent of Code repository!
+## Overview
+Advent of Code is a yearly event where a set of programming puzzles are released for the public to solve. I have decided to complete the 2024 year's challenges using Go programming language mainly to learn it. This is my first touch with this language.
+
+## Goals and Limitations
+- I will not be attempting to complete all the days and challenges.
+- My main priority is to provide working solutions that demonstrate my understanding of the concepts and techniques required to solve the puzzles.
+- I will also be documenting my thought process and the reasoning behind my choices in the code comments, to help others understand how I approached the problem.
+
+## Conclusion
+Please keep in mind that these solutions are my own and might not be the most optimal or the most beautiful, but they are the best I could come up with.
+
+## License
+This project is licensed under the terms of the MIT license.
